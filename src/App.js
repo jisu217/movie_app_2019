f9c82c945074f920a86b49c2fb64a6c1a16b1d96
jisu217 <ji_su217_@naver.com>
2025-07-20 13:21:21 +0900

    🎬 First commit: movie app project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./component/Navigation";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:idくsrc
+components
+# Movie.css
+Js Movie.js
+# Navigation.css
+Js Navigation.js
+routes
+# About.css
+Js About.js
+JS Detail.js
+# Home.css
+Js Home.js
+# App.css
+Js App.js
+Js index.js
+•gitignore
+i} package-lock.json
+1} package.json
+README.md" component={Detail} />
+    </HashRouter>
   );
 }
-
 export default App;
